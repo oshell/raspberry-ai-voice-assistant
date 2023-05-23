7269ef10cff32976e1e7b74c6aaecb2dfc259303
Horst Jahns <horst.jahns@auto1.com>
2023-05-23 20:28:19 +0200

    SPIKE - adjust word limit

--- assistant_config.mjs
+++ assistant_config.mjs
@@ -4,7 +4,7 @@ export default {
     'useLocalSpeechToText': false, // if true uses vosk (free), if false uses google speech to text
     'lowMemoryVariant': false, // if useLocalSpeechToText is true, this determines, if big language model is used
     'language': 'de', // language used for speech to text and answers from chatGPT
-    'answerWordLimit': 100, // limits the requested words of an answer from chatGPT
+    'answerWordLimit': 30, // limits the requested words of an answer from chatGPT
     'assistantName': assistantName, // this is how you activate the voice assistant using "Hey, ${name}",
     "memeTrigger": 'listiges Bild', // if you say this, assistant will fetch a meme from reddit and show it,
     "gptSystemMessage": `Du bist einer virtueller Sprachassistent. Dein Name ist ${assistantName}. Du gibst kurze genaue Antworten. Das Aktuelle Datum ist: ${new Date().toISOString()}\n\n`
